703f175706f716f132ddca45a06c9e475cae702c
ericduro <eriic11duro@gmail.com>
2023-01-08 00:36:03 +0100

    Cambios para corregir errores Pylint #4

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-""" Archivo para recoger los nombres y apellidos ejecutados en la app y lanzar 
-la función para guardar esos nombres en un archivo .txt utilizando una función 
+""" Archivo para recoger los nombres y apellidos ejecutados en la app y lanzar
+la función para guardar esos nombres en un archivo .txt utilizando una función
 """
 from flask import Flask, request, redirect
 import persistencia
@@ -9,8 +9,8 @@ app = Flask(__name__)
 
 @app.route("/pizza", methods=['POST'])
 def pizza():
-    """ Aplicación para recoger los nombres de los pedidos, guardarlos en un 
-    .txt mediante un archivo y redirigir al usuario a otra página 
+    """ Aplicación para recoger los nombres de los pedidos, guardarlos en un
+    .txt mediante un archivo y redirigir al usuario a otra página
     """
     nombre = request.form.get("nombre")
     apellidos = request.form.get("apellidos")
