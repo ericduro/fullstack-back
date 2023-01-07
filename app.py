0ad17f312ec39cb416ddc66dd30dc11f6045ad84
ericduro <eriic11duro@gmail.com>
2023-01-08 00:28:26 +0100

    Cambios para corregir errores Pylint

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+""" Archivo para recoger los nombres y apellidos ejecutados en la app y lanzar la función para guardar esos nombres en un archivo .txt utilizando una función """
 from flask import Flask, request, redirect
 import persistencia
 
@@ -6,8 +7,10 @@ app = Flask(__name__)
 
 @app.route("/pizza", methods=['POST'])
 def pizza():
+    """ Aplicación para recoger los nombres de los pedidos, guardarlos en un .txt mediante un archivo y redirigir al usuario a otra página """
     nombre = request.form.get("nombre")
     apellidos = request.form.get("apellidos")
     print ("Nombre: "+nombre+"\nApellidos: "+apellidos)
     persistencia.guardar_pedido(nombre, apellidos)
-    return redirect("http://localhost/M1_U1_ActividadFinal_EricDuroBasilio/solicita_pedido.html", code=302)
+    return redirect("http://localhost/M1_U1_ActividadFinal_EricDuroBasilio/solicita_pedido.html", 
+                    code=302)
--- persistencia.py
+++ persistencia.py
@@ -1,4 +1,6 @@
+""" Archivo para incluir la función para guardar pedidos """
 def guardar_pedido(nombre, apellidos):
+    """ Función para guardar los pedidos recibiendo nombre y apellidos como argumento """
     with open("pedidos.txt", "a", encoding="utf-8") as file:
         file.write(nombre + " " + apellidos + "\n")
         file.close()
