65836ffcd9257ce58c413a793f691ac6561021b6
ericduro <eriic11duro@gmail.com>
2023-01-08 01:01:10 +0100

    Cambios para corregir errores Pytest

--- persistencia.py
+++ persistencia.py
@@ -1,4 +1,4 @@
-""" Archivo para incluir la función para guardar pedidos con el guión delante """
+""" Archivo para la función para guardar pedidos con el guión delante """
 def guardar_pedido(nombre, apellidos):
     """ Función para guardar los pedidos recibiendo nombre y apellidos como argumento """
     with open("pedidos.txt", "a", encoding="utf-8") as file:
