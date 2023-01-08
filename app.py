24484bb11bfd1889f614cf8c60c4c2674a054e5b
ericduro <eriic11duro@gmail.com>
2023-01-08 11:57:49 +0100

    Comprobando disponibilidad del tamaño de la pizza

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """ Archivo para recoger los nombres y apellidos ejecutados en la app y lanzar
 la función para guardar esos nombres en un archivo .txt utilizando una función
 """
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, Response
 import persistencia
 
 app = Flask(__name__)
@@ -18,3 +18,15 @@ def pizza():
     persistencia.guardar_pedido(nombre, apellidos)
     return redirect("http://localhost/M1_U1_ActividadFinal_EricDuroBasilio/solicita_pedido.html",
     code=302)
+
+@app.route("/checksize",methods=['POST'])
+def checksize():
+    """
+    Comprueba disponibilidad de un tamaño de pizza.
+    """
+    size = request.form.get('tamano')
+    if size == 'S':
+        mensaje = "No disponible"
+    else:
+        mensaje = "Disponible"
+    return Response(mensaje, 200, {'Access-Control-Allow-Origin': '*'})
